0bea6c990750887f36a3510ed06557f5d817f8cf
Jay Barfiwala <47914155+JB9120@users.noreply.github.com>
2026-02-26 00:10:14 +0530

    Update parser.py

--- backend/parser.py
+++ backend/parser.py
@@ -1,5 +1,16 @@
-
 import dateparser
+from dateparser.search import search_dates
+from datetime import datetime
 
-def parse_datetime(text):
-    return dateparser.parse(text)
+def parse_datetime(text: str):
+    settings = {
+        "PREFER_DATES_FROM": "future",
+        "RELATIVE_BASE": datetime.now(),
+    }
+
+    result = search_dates(text, settings=settings)
+
+    if result:
+        return result[0][1]
+
+    return None
