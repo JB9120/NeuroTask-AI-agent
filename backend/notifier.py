05d7efe3285a21dbdb39281c774df393629ad96a
Jay Barfiwala <47914155+JB9120@users.noreply.github.com>
2026-02-27 23:50:46 +0530

    Delete backend directory

--- backend/notifier.py
+++
@@ -1,8 +0,0 @@
-
-from plyer import notification
-
-def notify(title, message):
-    try:
-        notification.notify(title=title, message=message, timeout=10)
-    except:
-        print(f"[NOTIFY] {title}: {message}")
