ebf703ec72eb20accc7ee6051f4ad94c271cf69f
Jay Barfiwala <47914155+JB9120@users.noreply.github.com>
2026-03-01 22:44:07 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,10 +9,43 @@ export default function Login() {
   const [password,setPassword]=useState("")
 
   const login = async () => {
-    const res = await api.post("/auth/login",{email,password})
+
+  try {
+
+    setLoading(true)
+    setError("")
+
+    const formData = new URLSearchParams()
+
+    formData.append("username", email)
+    formData.append("password", password)
+
+    const res = await api.post(
+      "/auth/login",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
+    )
+
     loginStore(res.data.access_token)
-    window.location="/"
+
+    window.location = "/"
+
   }
+  catch(err){
+
+    setError(
+      err.response?.data?.detail ||
+      "Login failed"
+    )
+  }
+  finally{
+    setLoading(false)
+  }
+}
 
   return (
     <div>
